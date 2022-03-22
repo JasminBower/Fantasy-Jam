1435ed63caf3531a75b9bd5087a4e4f7b15b6a87
andre <andrecosta8@gmail.com>
2022-03-22 16:56:04 +0000

    match ids with database

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,35 +1,40 @@
 const isLoggedIn = require("../middleware/isLoggedIn");
 const router = require("express").Router();
-const User = require("../models/User.model");
+const User = require('../models/User.model');
 const Driver = require("../models/Driver.model");
-const Team = require("../models/Team.model");
-const axios = require("axios");
+const Team = require('../models/Team.model');
+const axios = require('axios');
+const getRandom = require("../utils/getRandom");
 
 let driversApi = axios.get("http://ergast.com/api/f1/2022/driverStandings.json");
 
 router.get("/profile", isLoggedIn, (req, res) => {
-	res.render("profile/profile", { user: req.session.user });
+  res.render("profile/profile", { user: req.session.user });
 });
 
+router.get("/getdrivers", isLoggedIn, async (req, res) => {
+  let teamArray = [];
+  randomDriver = await driversApi;
+  let randomResult =  randomDriver.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[getRandom(20)]
+  // let randomResult2 = randomDriver.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[getRandom(20)]
+  // let randomResult3 = randomDriver.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[getRandom(20)]
+  teamArray.push(randomResult.Driver.familyName)
+  console.log(randomResult);
+  res.render("profile/getdrivers", {randomResult});
+  // const button1 = document.getElementById("button").addEventListener("click", randomResult);
+  // const button2 = document.getElementById("button2").addEventListener("click", randomResult2);
+  // const button3 = document.getElementById("button3").addEventListener("click", randomResult3);
+ });
 
-router.get("/getdrivers",isLoggedIn, async (req,res) => {
-   
-    res.render("profile/getdrivers");
+ router.post("/getdrivers", isLoggedIn, async (req, res) => {
+  console.log(req.session)
+  const newTeam = {username:req.session.user.username, driverId: req.body.driverId}
+  Team.create(newTeam);
+  res.redirect("/profile",);
+  });
 
 
-});
-router.get("/randomdriver",isLoggedIn, async (req,res) => {
-  res.render("profile/randomdriver");
-});
 
-router.post("/randomdriver",isLoggedIn, async (req,res) => {
-randomDriver = await driversApi;
-let randNum = Math.floor(Math.random() * 19)
-console.log(randomDriver.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[randNum].Driver);
-let randomResult = randomDriver.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[randNum]//.points//.Driver
-//let randomResult = randomDriver.data.MRData.DriverTable.Drivers[randNum];
-res.render("profile/randomdriver", {randomResult});
-});
 
 
 module.exports = router;
