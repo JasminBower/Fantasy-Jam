9f26b65eec09ef3ae92d56a794fbea79a085607a
JasminBower <jasmin.ibower@gmail.com>
2022-03-24 14:59:23 +0100

    refresh bug fixed for scoreboard comments

--- routes/scoreboard.routes.js
+++ routes/scoreboard.routes.js
@@ -9,6 +9,7 @@ const findWinner = require("../utils/findWinner");
 // *** highlight current user position
 
 router.get("/scoreboard", async (req, res, next) => {
+	let allComments = await Comment.find();
 	let teams = await Team.find();
 	let sortedTeams = teams.sort((a, b) => {
 		return b.teamScore - a.teamScore;
@@ -16,19 +17,19 @@ router.get("/scoreboard", async (req, res, next) => {
 
 	const winner = sortedTeams[0].username;
 
-	res.render("auth/scoreboard", { sortedTeams, winner });
+	res.render("auth/scoreboard", { sortedTeams, winner, allComments });
 });
 
 router.post("/scoreboard", async (req, res, next) => {
 	const { comment } = req.body;
 	let newComment = {
-		username,
+		username: req.session.user.username,
 		comment: comment,
 	};
-
+	console.log(newComment);
 	let comments = await Comment.create(newComment);
 
-	res.render("auth/scoreboard", comments);
+	res.redirect("/scoreboard");
 });
 
 module.exports = router;
