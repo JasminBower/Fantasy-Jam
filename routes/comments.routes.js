0e7174bf8a8969bd273fc704537fe3a4b5c5d4a6
JasminBower <jasmin.ibower@gmail.com>
2022-03-24 18:56:43 +0100

    comments almost working

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -2,15 +2,27 @@ const router = require("express").Router();
 const Team = require("../models/Team.model");
 const Comment = require("../models/Comment.model");
 
-router.get("/comments/:id", (req, res, next) => {
+router.get("/comments/:id", async (req, res, next) => {
 	let username = req.session.user.username;
-	console.log(req.params, "GGGETTTTTT");
+	//console.log(req.params, "<<<");
+	let { id } = req.params;
+	let comment = await Comment.findById(id);
+	//console.log(comment, "CHEEEESE");
 
-	res.render("comments", { username });
+	res.render("comments", { username, comment });
 });
 
-router.post("/comments", (req, res, next) => {
-	console.log(req.body, "POSSSSSRRRRRRRRR");
+router.post("/comments/:id", async (req, res, next) => {
+	console.log("Hiiiiiiii");
+	let { comment } = req.body;
+	let { article_id } = req.params;
+	let { id } = req.params;
+
+	console.log(req.params, "<<< Paramassszzzzzz");
+	console.log(comment, "IIIIIIIIIII");
+
+	await Comment.findOneAndUpdate({ id }, { comment }, { new: true });
+
 	res.redirect("/scoreboard");
 });
 
