663ebeddc7b476072984e6394991c5f267e8426e
JasminBower <jasmin.ibower@gmail.com>
2022-05-09 23:00:06 +0200

    added to connect to heroku and mongodb atlas

--- db/index.js
+++ db/index.js
@@ -7,13 +7,15 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = require("../utils/consts");
 
+require("dotenv").config();
+
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+	.connect(process.env.MONGODB_URI)
+	.then((x) => {
+		console.log(
+			`Connected to Mongo! Database name: "${x.connections[0].name}"`,
+		);
+	})
+	.catch((err) => {
+		console.error("Error connecting to mongo: ", err);
+	});
